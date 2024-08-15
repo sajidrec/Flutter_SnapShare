7bdb8f7e1f52af1460e28a2ddf14e607268b38f3
RAKIB <rakibulislam10.net@gmail.com>
2024-08-15 15:53:49 +0600

    login screen issue fix

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -6,13 +6,33 @@ import 'package:snapshare/presentation/screens/bottom_nav_bar.dart';
 import 'package:snapshare/widgets/check_box.dart';
 import 'package:snapshare/widgets/text_field.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final TextEditingController emailController = TextEditingController();
+  State<LoginScreen> createState() => _LoginScreenState();
+}
 
+class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late TextEditingController emailController = TextEditingController();
+  late TextEditingController passwordController = TextEditingController();
+  bool isButtonActive = false;
+
+  @override
+  void initState() {
+    super.initState();
+    passwordController = TextEditingController();
+    passwordController.addListener(() {
+      final isButtonActive = passwordController.text.isNotEmpty;
+      setState(() {
+        this.isButtonActive = isButtonActive;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -25,46 +45,74 @@ class LoginScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(30),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 64),
-              const Text(
-                "Enter your phone number\n and password",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
-              ),
-              const SizedBox(height: 32),
-              const Text('Email',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-              const SizedBox(height: 10),
-              TextFields(
-                  hintText: 'Email',
-                  icon: const Icon(Icons.email_outlined),
-                  controller: emailController),
-              const SizedBox(height: 15),
-              const Text('Password',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-              const SizedBox(height: 10),
-              TextFields(
-                  isPassword: true,
-                  hintText: 'Password',
-                  icon: const Icon(Icons.lock_open_outlined),
-                  controller: emailController),
-              const SizedBox(height: 10),
-              const CheckBox(),
-              const SizedBox(height: 18),
-              SizedBox(
-                width: double.maxFinite,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Get.offAll(() => const BottomNavBar());
-                  },
-                  child: const Text("Login"),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 64),
+                const Text(
+                  "Enter your phone number\n and password",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
                 ),
-              ),
-              const SizedBox(height: 10),
-              _buildTextButton()
-            ],
+                const SizedBox(height: 32),
+                const Text('Email',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                const SizedBox(height: 10),
+                TextFields(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                          .hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                    hintText: 'Email',
+                    icon: const Icon(Icons.email_outlined),
+                    controller: emailController),
+                const SizedBox(height: 15),
+                const Text('Password',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                const SizedBox(height: 10),
+                TextFields(
+                    validator: (value) {
+                      if (value == null || value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                    isPassword: true,
+                    hintText: 'Password',
+                    icon: const Icon(Icons.lock_open_outlined),
+                    controller: passwordController),
+                const SizedBox(height: 10),
+                const CheckBox(),
+                const SizedBox(height: 18),
+                SizedBox(
+                  width: double.maxFinite,
+                  child: ElevatedButton(
+                    onPressed: isButtonActive
+                        ? () {
+                            setState(() {
+                              isButtonActive = false;
+                              passwordController.clear();
+                              if (_formKey.currentState!.validate()) {
+                                Get.offAll(const BottomNavBar());
+                              }
+                            });
+                          }
+                        : null,
+                    child: const Text("Login"),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                _buildTextButton()
+              ],
+            ),
           ),
         ),
       ),
@@ -94,5 +142,12 @@ class LoginScreen extends StatelessWidget {
         )
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 }
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class AppColor {
   static const Color themeColor = Color(0xFF4478FF);
   static const Color inputBorderColor = Color(0xFF6993FF);
