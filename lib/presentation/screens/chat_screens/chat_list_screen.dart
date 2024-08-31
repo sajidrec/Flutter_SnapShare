a0f3af9d53235b003cc52ec64363558dd963c260
backward <shazzadshojol@gmail.com>
2024-08-31 23:06:01 +0600

    SCRUM-48 dark mode done message screen. and modification done on chat and message screen.

--- lib/presentation/screens/chat_screens/chat_list_screen.dart
+++ lib/presentation/screens/chat_screens/chat_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:snapshare/presentation/controller/chat_list_screen_controller.dart';
-import 'package:snapshare/presentation/screens/chat_screen.dart';
+import 'package:snapshare/presentation/controller/chat_controller/chat_list_screen_controller.dart';
+import 'package:snapshare/presentation/screens/chat_screens/chat_screen.dart';
 import 'package:snapshare/utils/app_colors.dart';
 
 class ChatListScreen extends StatefulWidget {
@@ -23,8 +23,21 @@ class _ChatListScreenState extends State<ChatListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final customColor = AppColor.forText(context);
+
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Users List',
+            style: TextStyle(color: customColor),
+          ),
+          centerTitle: true,
+          automaticallyImplyLeading: true,
+          iconTheme: IconThemeData(
+            color: customColor,
+          ),
+        ),
         body: GetBuilder<ChatListScreenController>(
           builder: (chatListScreenController) {
             return chatListScreenController.inProgress
@@ -38,13 +51,13 @@ class _ChatListScreenState extends State<ChatListScreen> {
                         const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
                     child: (chatListScreenController
                             .getListOfMessagesUser.isEmpty)
-                        ? const Center(
+                        ? Center(
                             child: Text(
                               "No chat yet",
                               style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 16,
-                              ),
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16,
+                                  color: customColor),
                             ),
                           )
                         : ListView.separated(
@@ -52,6 +65,7 @@ class _ChatListScreenState extends State<ChatListScreen> {
                                 .getListOfMessagesUser.length,
                             itemBuilder: (context, index) {
                               return _buildChatElement(
+                                color: customColor,
                                 chatListScreenController:
                                     chatListScreenController,
                                 index: index,
@@ -81,44 +95,52 @@ class _ChatListScreenState extends State<ChatListScreen> {
     );
   }
 
-  Widget _buildChatElement({
-    required ChatListScreenController chatListScreenController,
-    required int index,
-    required VoidCallback onTap,
-  }) {
+  Widget _buildChatElement(
+      {required ChatListScreenController chatListScreenController,
+      required int index,
+      required VoidCallback onTap,
+      required Color color}) {
     return InkWell(
       onTap: onTap,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              CircleAvatar(
-                radius: Get.width / 15,
-                backgroundImage: NetworkImage(
-                  chatListScreenController
-                          .getListOfMessagesUser[index].userPhoto ??
-                      "",
-                ),
-              ),
-              const SizedBox(width: 5),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: Get.width / 15,
+                  backgroundImage: NetworkImage(
                     chatListScreenController
-                            .getListOfMessagesUser[index].userFullName ??
+                            .getListOfMessagesUser[index].userPhoto ??
                         "",
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w700),
                   ),
-                  Text(
-                      "@${chatListScreenController.getListOfMessagesUser[index].username ?? ""}"),
-                ],
-              ),
-            ],
-          ),
-        ],
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      chatListScreenController
+                              .getListOfMessagesUser[index].userFullName ??
+                          "",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: color),
+                    ),
+                    SizedBox(height: 3),
+                    Text(
+                      "@${chatListScreenController.getListOfMessagesUser[index].username ?? ""}",
+                      style: TextStyle(color: color),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
